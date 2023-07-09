aeb232f5d0e743b284fa7f06090266022773cc3e
Ekaterina Bakaeva <bakaeva.es@gmail.com>
2023-07-09 14:01:27 +0300

    TimeStamp fixed. Readable code

--- js/hall.js
+++ js/hall.js
@@ -1,9 +1,19 @@
-console.log(localStorage);
-const stamp = localStorage.getItem('timeStamp');
-createRequest(`"event=get_hallConfig&timestamp=${stamp}&hallId=${localStorage.getItem('hallId')}&seanceId=${localStorage.getItem('seanceId')}"`, (response) => {
-    console.log(response)
+createRequest(`event=get_hallConfig&timeStamp=${localStorage.getItem('timeStamp')}&hallId=${localStorage.getItem('hallId')}&seanceId=${localStorage.getItem('seanceId')}`, (response) => {
+    console.log(JSON.parse(response));
+})
+
+const place = Array.from(document.querySelectorAll(".conf-step__chair "));
+
+place.forEach(item => {
+    item.addEventListener("click", event => {
+        if (!item.classList.contains("conf-step__chair_selected")){
+        item.classList.add("conf-step__chair_selected");
+        } else {
+            item.classList.remove("conf-step__chair_selected");
+        }
+    })
 })
 
 
 
-
+//const wrapper = document.querySelector(".conf-step__wrapper"); 
--- js/index.js
+++ js/index.js
@@ -1,59 +1,61 @@
-
-const mainBlock = document.querySelector("main"); 
+const mainBlock = document.querySelector("main");
 mainBlock.innerHTML = "";
 const daysNav = Array.from(document.querySelectorAll(".page-nav__day"));
-const timeStamp = new Date().getTime();
+let time = new Date()
+time.setHours(00, 00, 00);
+let timeStamp = time.getTime();
 //time.setHours(00,00,00) //текущий таймстэмп
- for (let i=0; i<daysNav.length; i++){
-    let dateNav = timeStamp + (24 * 60 * 60 * 1000 * i)
-    let dateFormat = new Date(dateNav);
-    let oneDay = daysNav[i];
-    oneDay.dataset.timeStamp = dateNav;
+for (let i = 0; i < daysNav.length; i++) {
+	let dateNav = timeStamp + (24 * 60 * 60 * 1000 * i);
+	let dateFormat = new Date(dateNav);
+	let oneDay = daysNav[i];
+	oneDay.dataset.timeStamp = dateNav;
 
-    //каждому элементу присваиваем таймтэмп: таймстэмп + сутки * на индекс элемента
-    // Считаем дату и меняем в дочерних элементах текстконтент
-    // Если СБ или ВС - тоглаем класс page-nav__day_weekend
-    const dateWeek = (oneDay.getElementsByClassName("page-nav__day-week"))[0];      
-    const dateNumber = (oneDay.getElementsByClassName("page-nav__day-number"))[0];
-    
-    function getWeekDay(date) {
-        let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
-      
-        return days[date.getDay()];
-      }
-      dateWeek.textContent = String(getWeekDay(dateFormat));
-      dateNumber.textContent = String(dateFormat.getDate());
-      if (dateWeek.textContent === "ВС" || dateWeek.textContent === "СБ"){
-        oneDay.classList.add("page-nav__day_weekend")
-      }
-    // вещаем прослушку на клик: по клику убираем у другого элемента и добавляем этому класс page-nav__day_chosen
-    //
-    daysNav[i].addEventListener("click", event => {
-        daysNav.forEach(item => {
-            if(item.classList.contains("page-nav__day_chosen")) {
-                item.classList.remove("page-nav__day_chosen")
-            }
-        })
-        daysNav[i].classList.add("page-nav__day_chosen");
-        event.preventDefault();
-    })
+	//каждому элементу присваиваем таймтэмп: таймстэмп + сутки * на индекс элемента
+	// Считаем дату и меняем в дочерних элементах текстконтент
+	// Если СБ или ВС - тоглаем класс page-nav__day_weekend
+	const dateWeek = (oneDay.getElementsByClassName("page-nav__day-week"))[0];
+	const dateNumber = (oneDay.getElementsByClassName("page-nav__day-number"))[0];
+
+	function getWeekDay(date) {
+		let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+
+		return days[date.getDay()];
+	}
+	dateWeek.textContent = String(getWeekDay(dateFormat));
+	dateNumber.textContent = String(dateFormat.getDate());
+	if (dateWeek.textContent === "ВС" || dateWeek.textContent === "СБ") {
+		oneDay.classList.add("page-nav__day_weekend")
+	}
+	// вещаем прослушку на клик: по клику убираем у другого элемента и добавляем этому класс page-nav__day_chosen
+	//
+	daysNav[i].addEventListener("click", event => {
+		daysNav.forEach(item => {
+			if (item.classList.contains("page-nav__day_chosen")) {
+				item.classList.remove("page-nav__day_chosen")
+			}
+		})
+		daysNav[i].classList.add("page-nav__day_chosen");
+		console.log(document.querySelector(".page-nav__day_chosen").getAttribute("data-time-stamp"))
+		event.preventDefault();
+	})
 
 
 }
 
 
 createRequest("event=update", (response) => {
-response.films.result.forEach(item => {
-    const filmDescription = item.film_description;
-    const filmDuration = item.film_duration;
-    const filmId = item.film_id;
-    const filmName = item.film_name;
-    const filmOrigin = item.film_origin;
-    const filmPoster = item.film_poster;
+	response.films.result.forEach(item => {
+		const filmDescription = item.film_description;
+		const filmDuration = item.film_duration;
+		const filmId = item.film_id;
+		const filmName = item.film_name;
+		const filmOrigin = item.film_origin;
+		const filmPoster = item.film_poster;
 
 
-mainBlock.insertAdjacentHTML("beforeEnd",
-    `<section class="movie">
+		mainBlock.insertAdjacentHTML("beforeEnd",
+			`<section class="movie">
     <div class="movie__info">
       <div class="movie__poster">
         <img class="movie__poster-image" alt="${filmName} постер" src=${filmPoster}>
@@ -62,84 +64,52 @@ mainBlock.insertAdjacentHTML("beforeEnd",
         <h2 class="movie__title">${filmName}</h2>
         <p class="movie__synopsis">${filmDescription}</p>
         <p class="movie__data">
-          <span class="movie__data-duration">${filmDuration}т</span>
+          <span class="movie__data-duration">${filmDuration} мин</span>
           <span class="movie__data-origin">${filmOrigin}</span>
         </p>
       </div>
     </div>  
   </section>`
-)
-const lastMovie = mainBlock.lastChild;
-const seancesArr = response.seances.result.filter(seance => seance.seance_filmid === filmId);
-const hallsArr = response.halls.result;
-const hallCounter = [];
-for (let i=0; i<seancesArr.length; i++) {
-     if (hallCounter.includes(seancesArr[i].seance_hallid)){
-       const hallExist = lastMovie.querySelector(`[data-hall-id="${seancesArr[i].seance_hallid}"]`).lastElementChild;
-       hallExist.insertAdjacentHTML("beforeEnd",
-       `<li class="movie-seances__time-block" data-seance-time-stamp="${seancesArr[i].seance_start}" data-seance-id="${seancesArr[i].seance_id}"><a class="movie-seances__time" href="hall.html">${seancesArr[i].seance_time}</a></li>`)
-    }else{
-    hallCounter.push(seancesArr[i].seance_hallid);
-    const hallName = hallsArr.filter(hall => hall.hall_id === seancesArr[i].seance_hallid);
-    const hallNameNumber = (`${hallName[0].hall_name}`).slice(3,4)
-    lastMovie.insertAdjacentHTML("beforeEnd",
-    `<div class="movie-seances__hall" data-hall-id="${seancesArr[i].seance_hallid}">
+		)
+		const lastMovie = mainBlock.lastChild;
+		const seancesArr = response.seances.result.filter(seance => seance.seance_filmid === filmId);
+		const hallsArr = response.halls.result;
+		const hallCounter = [];
+		for (let i = 0; i < seancesArr.length; i++) {
+			if (hallCounter.includes(seancesArr[i].seance_hallid)) {
+				const hallExist = lastMovie.querySelector(`[data-hall-id="${seancesArr[i].seance_hallid}"]`).lastElementChild;
+				hallExist.insertAdjacentHTML("beforeEnd",
+					`<li class="movie-seances__time-block" data-seance-time-stamp="${seancesArr[i].seance_start}" data-seance-id="${seancesArr[i].seance_id}"><a class="movie-seances__time" href="hall.html">${seancesArr[i].seance_time}</a></li>`)
+			} else {
+				hallCounter.push(seancesArr[i].seance_hallid);
+				const hallName = hallsArr.filter(hall => hall.hall_id === seancesArr[i].seance_hallid);
+				const hallNameNumber = (`${hallName[0].hall_name}`).slice(3, 4)
+				lastMovie.insertAdjacentHTML("beforeEnd",
+					`<div class="movie-seances__hall" data-hall-id="${seancesArr[i].seance_hallid}">
       <h3 class="movie-seances__hall-title" >Зал ${hallNameNumber}</h3>
       <ul class="movie-seances__list">
         <li class="movie-seances__time-block" data-seance-time-stamp="${seancesArr[i].seance_start}" data-seance-id="${seancesArr[i].seance_id}"><a class="movie-seances__time" href="hall.html">${seancesArr[i].seance_time}</a></li>    
       </ul>
     </div>`)
 
-}
-}
+			}
+		}
+	})
+
+	const seanceTime = Array.from(document.querySelectorAll(".movie-seances__time"));
+	seanceTime.forEach(item => {
+		item.addEventListener("click", event => {
+			localStorage.clear();
+			let timeStampDay = document.querySelector(".page-nav__day_chosen").getAttribute("data-time-stamp");
+			let timeStampSeance = item.closest(".movie-seances__time-block").getAttribute("data-seance-time-stamp");
+			let hallIdSeance = item.closest(".movie-seances__hall").getAttribute("data-hall-id");
+			let chosenSeanceId = item.closest(".movie-seances__time-block").getAttribute("data-seance-id");
+			let filmTimeStamp = Math.trunc(Number(timeStampDay) / 1000) + (Number(timeStampSeance) * 60);
+			console.log(timeStampSeance)
+			localStorage.setItem("timeStamp", filmTimeStamp);
+			localStorage.setItem("hallId", hallIdSeance);
+			localStorage.setItem("seanceId", chosenSeanceId);
+		})
+	})
+
 })
-
-const seanceTime = Array.from(document.querySelectorAll(".movie-seances__time"));
-seanceTime.forEach(item => {
-    item.addEventListener("click", event => {
-      localStorage.clear();
-        let timeStampDay = document.querySelector(".page-nav__day_chosen").getAttribute("data-time-stamp");
-        let timeStampSeance = item.closest(".movie-seances__time-block").getAttribute("data-seance-time-stamp");
-        let hallIdSeance = item.closest(".movie-seances__hall").getAttribute("data-hall-id");
-        let chosenSeanceId = item.closest(".movie-seances__time-block").getAttribute("data-seance-id");
-        let filmTimeStamp = (Math.trunc(Number(timeStampDay) / 1000) + (Number(timeStampSeance) * 60));
-
-        localStorage.setItem("timeStamp",filmTimeStamp);
-        localStorage.setItem("hallId", hallIdSeance);
-        localStorage.setItem("seanceId", chosenSeanceId);
-})
-})
-
-}
-)
-
-
-// сохранить в локал инфу о выбранном сеансе: название, Зал, сеанс
-// const seanceTime = Array.from(document.querySelectorAll(".movie-seances__time"));
-// console.log(seanceTime)
-// seanceTime.forEach(item => {
-//     item.addEventListener("click", event => {
-//         let timeStampDay = document.querySelector(".page-nav__day_chosen").getAttribute("data-time-stamp");
-//         let timeStampSeance = item.closest(".movie-seances__time-block").getAttribute("data-seance-time-stamp");
-//         let hallIdSeance = item.closest(".movie-seances__hall").getAttribute("data-hall-id");
-//         let chosenSeanceId = item.item.closest(".movie-seances__time-block").getAttribute("data-seance-id");
-//         let filmTimeStamp = timeStampDay + (timeStampSeance * 60);
-
-//         console.log(filmTimeStamp)
-
-
-//         localStorage.setItem(`"timeStamp","${filmTimeStamp}"`);
-//         localStorage.setItem("hallId", hallIdSeance);
-//         localStorage.setItem("seanceId", chosenSeanceId);
-        
-// event.preventDefault()
-// })
-// })
-
-//20 минута
-//текущий таймстэмп
-//const timeStamp 
-// для других дней прибавляем количество часов * количество минут * количество секунд, сохранить в дата=атрибуте data-time-stamp="45354534"
-// ищем вкладку  с активным классом относительного него раскидываем значения сеансов
-// сохранить в локал инфу и передать дальше
-//const wrapper = document.querySelector(".conf-step__wrapper"); 
